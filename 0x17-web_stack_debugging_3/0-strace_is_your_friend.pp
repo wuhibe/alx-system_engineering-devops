9d6056cd42fb2959657e09894673083fd6b0d701
wuhibe <wuhibetamire710@gmail.com>
2022-02-16 23:38:23 +0300

    Revert "first commit"
    This reverts commit 424b6663d158c5dba1ba40a2cac17d45de9c00b3.

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++
@@ -1,6 +0,0 @@
-# remove extra p in config file and add env variables to source
-exec { 'typo and envvars':
-  command => "sed -i 's/phpp/php/' /var/www/html/wp-settings.php;
-  	      source /etc/apache2/envvars",
-  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-}
