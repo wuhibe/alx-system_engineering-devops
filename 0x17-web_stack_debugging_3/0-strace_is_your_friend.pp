465c4cc11c85731fec02f2579f6f928a244e681c
wuhibe <wuhibetamire710@gmail.com>
2022-02-16 23:27:11 +0300

    0

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,9 +1,6 @@
 # remove extra p in config file and add env variables to source
-exec { 'typo':
-  command => "sed -i 's/phpp/php/' /var/www/html/wp-settings.php",
-  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-}
-exec { 'envvars':
-  command => source /etc/apache2/envvars',
+exec { 'typo and envvars':
+  command => "sed -i 's/phpp/php/' /var/www/html/wp-settings.php;
+  	      source /etc/apache2/envvars",
   path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
 }
