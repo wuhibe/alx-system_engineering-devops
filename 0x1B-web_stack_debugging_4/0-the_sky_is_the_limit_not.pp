32b014bc4289d845084eea62cf17ed8bf15f2c44
wuhibe <wuhibetamire710@gmail.com>
2022-02-24 18:58:23 +0300

    further than the moon

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,6 @@
 # increases the nginx limit to 2000
 exec { 'fix file limit':
   onlyif  => 'test -e /etc/default/nginx',
-  command => 'sed -i 's/15/2000/' /etc/default/nginx; service nginx restart',
+  command => 'sed -i 's/15/4096/' /etc/default/nginx; service nginx restart',
   path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
 }
