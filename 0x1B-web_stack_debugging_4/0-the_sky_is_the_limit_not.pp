7c42a2115c03a1fa5ead6ec00ce9fd97112d317e
wuhibe <wuhibetamire710@gmail.com>
2022-02-24 19:00:57 +0300

    last conf

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,6 @@
-# increases the nginx limit to 2000
+# increases the nginx limit to 15k
 exec { 'fix file limit':
   onlyif  => 'test -e /etc/default/nginx',
-  command => 'sed -i 's/15/4096/' /etc/default/nginx; service nginx restart',
+  command => 'sed -i 's/15/15000/' /etc/default/nginx; service nginx restart',
   path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
 }
